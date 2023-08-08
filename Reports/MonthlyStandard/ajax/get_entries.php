f3d92094b5cc3e23b5c85c0bf9b2dda324ff6b25
Joshua Mari P. Coquia <coquia-kdt@kdts.net>
2023-08-08 15:14:42 +0800

    fix get entries

--- Reports/MonthlyStandard/ajax/get_entries.php
+++ Reports/MonthlyStandard/ajax/get_entries.php
@@ -9,40 +9,39 @@ date_default_timezone_set('Asia/Manila');
 
 #region initialize variables
 $yearMonth = date("Y-04");
-if(!empty($_POST['monthSel'])){
+if (!empty($_POST['monthSel'])) {
     $yearMonth = $_POST['monthSel'];
 }
 $empStatement = "";
 $employeeArray = array();
-if(!empty($_POST['empArray'])){
+if (!empty($_POST['empArray'])) {
     $employeeArray = $_POST['empArray'];
-    $implodeString = implode("','",$employeeArray);
-    $empStatement="AND dr.fldEmployeeNum IN ('" . $implodeString . "')";
+    $implodeString = implode("','", $employeeArray);
+    $empStatement = "AND dr.fldEmployeeNum IN ('" . $implodeString . "')";
 }
 $entriesArray = array();
 #endregion
 
 #region main
-$entriesQuery = "SELECT dr.fldProject AS projID, pt.fldProject AS projName, dr.fldEmployeeNum AS eNum, dr.fldDate AS eDate, dr.fldDuration AS eMinutes, dr.fldMHType AS eMHT, dr.fldItem AS itemID FROM dailyreport AS dr JOIN projectstable AS pt ON dr.fldProject = pt.fldID WHERE dr.fldDate LIKE :yearMonth $empStatement";
-$entriesStmt = $connwebjmr -> prepare($entriesQuery);
-$entriesStmt -> execute([":yearMonth" => "$yearMonth%"]);
-if($entriesStmt -> rowCount()>0){
-    $entriesArr = $entriesStmt -> fetchAll();
-    foreach($entriesArr AS $entries){
+$entriesQuery = "SELECT dr.fldProject AS projID, pt.fldProject AS projName, dr.fldEmployeeNum AS eNum, dr.fldDate AS eDate, SUM(dr.fldDuration) AS projMinute, dr.fldMHType AS eMHT, dr.fldItem AS itemID FROM dailyreport AS dr JOIN projectstable AS pt ON dr.fldProject = pt.fldID WHERE dr.fldDate LIKE :yearMonth $empStatement GROUP BY dr.fldProject,dr.fldMHType";
+$entriesStmt = $connwebjmr->prepare($entriesQuery);
+$entriesStmt->execute([":yearMonth" => "$yearMonth%"]);
+if ($entriesStmt->rowCount() > 0) {
+    $entriesArr = $entriesStmt->fetchAll();
+    foreach ($entriesArr as $entries) {
         $output = array();
         $output += ["pIndex" => $entries['projID']];
         $output += ["pName" => $entries['projName']];
         $output += ["empNum" => $entries['eNum']];
         $rawDate = $entries['eDate'];
-        $entryDay = date("d",strtotime($rawDate));
+        $entryDay = date("d", strtotime($rawDate));
         $output += ["entryDate" => $entryDay];
-        $rawMinutes = $entries['eMinutes'];
+        $rawMinutes = $entries['projMinute'];
         $entryHours = $rawMinutes / 60;
         $output += ["hours" => $entryHours];
         $output += ["OT" => ($entries['eMHT'] == 1) ? TRUE : FALSE];
         $output += ["iIndex" => $entries['itemID']];
-        array_push($entriesArray,$output);
-
+        array_push($entriesArray, $output);
     }
 }
 #endregion
