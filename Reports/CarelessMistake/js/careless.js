452bfd4eeba1dcef30b1c516656c24787a7b033b
Collene Keith Medrano <medrano_c-kdt@kdts.net>
2023-08-25 13:23:09 +0800

    initial table cm report

--- Reports/CarelessMistake/js/careless.js
+++ Reports/CarelessMistake/js/careless.js
@@ -10,3 +10,34 @@ switch (document.location.hostname) {
     rootFolder = "//kdt-ph/";
     break;
 }
+
+$(document).ready(function () {});
+$(document).on("click", ".bu", function () {
+  getID = $(this).attr("id");
+
+  // $(`.${getID}.collapse`).removeClass("collapse");
+  // $(`.${getID}.collapse`).addClass("collapsing");
+  // $(`.${getID}.collapse`).removeClass("collapsing");
+  $(`.${getID}.collapse`).toggleClass("show");
+});
+var tary = `<tr class="collapse bu2 show">
+<td>Employee B</td>
+<td>40</td>
+<td></td>
+<td></td>
+<td>20</td>
+<td>50%</td>
+<td></td>
+<td></td>
+<td>20</td>
+<td>50%</td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>`;
+$(tary).insertBefore(".total");
