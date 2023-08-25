82dcb1628981f3777ffa2ea2764841f457167f9a
Felix Edwin C. Petate <felix@kdts.net>
2023-08-25 15:25:32 +0800

    planning report draft

--- Reports/PlannedMHReport/js/newtable.js
+++ Reports/PlannedMHReport/js/newtable.js
@@ -37,6 +37,22 @@ Date.prototype.yyyymmdd = function () {
   ].join('-');
 };
 
+function roundToDecimalPlaces(number, decimalPlaces) {
+  const factor = 10 ** decimalPlaces;
+  return Math.round(number * factor) / factor;
+};
+
+function makeArrayUnique(arr, key) {
+  const uniqueValues = new Set();
+  return arr.filter(item => {
+    if (!uniqueValues.has(item[key])) {
+      uniqueValues.add(item[key]);
+      return true;
+    }
+    return false;
+  });
+};
+
 //#endregion
 
 $(document).ready(function () {
@@ -44,9 +60,11 @@ $(document).ready(function () {
   const getToday = new Date();
   $('#monthSel').val(`${getToday.getFullYear()}-${(parseInt(getToday.getMonth()) + 1).toString().padStart(2, '0')}`)
 
-  queryfunctions(); //functions para sa pag get ng data + create table
+  queryfunctions();
 
 });
+
+//#region Controls
 
 $(document).on('click', '#clearWeek', function () {
   clearWeek();
@@ -63,6 +81,9 @@ $(document).on('change', '#weekSel', function () {
   validateWeekRange($('#monthSel').val());
 })
 
+//#endregion
+
+//#region mga query
 function queryfunctions() {
   $('#main-tbody').empty()
 
@@ -82,16 +103,16 @@ function queryfunctions() {
     }
   );
 
-  //queries
+  //DR
   $.getJSON("js/queries.json",
     function (data) {
       data.map(latagActual);
     }
   );
-
-  //color weekend
 }
+//#endregion
 
+//#region table sa taas
 function createTable(mVal) {
   var firstDay = new Date(`${mVal}-01`);
   var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth() + 1, 0)
@@ -138,50 +159,9 @@ function latagDays(fd, ld) {
   $('.plan-row, .actual-row').append(addCells)
 }
 
-function validateWeekRange(mVal) {
-
-  var firstDay = new Date(`${mVal}-01`);
-  var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth() + 1, 0)
-
-  const selectedValue = $('#weekSel').val();
-
-  const selectedWeek = parseInt(selectedValue.slice(6));
-
-  // Define your allowed range
-  const allowedWeekRange = [getWeekNo(firstDay), getWeekNo(lastDay)]; // Adjust as needed
-
-  if (
-    selectedWeek >= allowedWeekRange[0] &&
-    selectedWeek <= allowedWeekRange[1]
-  ) {
-    highlightweek(selectedValue)
-  } else {
-    clearWeek()
-    alert('Selected week is outside this month\'s range.');
-  }
-}
-
-function getWeekNo(currentDate) {
-  var startDate = new Date(currentDate.getFullYear(), 0, 1);
-  var days = Math.floor((currentDate - startDate) /
-    (24 * 60 * 60 * 1000));
-  var weekNumber = Math.ceil(days / 7);
-  return weekNumber;
-}
-
-function clearWeek() {
-  $('#weekSel').val(0)
-}
-
 function latagProjects(data) {
-  const uniquePNums = new Set();
-  const filteredData = data.filter(item => {
-    if (!uniquePNums.has(item.pNum)) {
-      uniquePNums.add(item.pNum);
-      return true;
-    }
-    return false;
-  });
+
+  const filteredData = makeArrayUnique(data, 'pNum');
 
   filteredData.forEach(element => {
     $('#main-tbody').append(`
@@ -229,7 +209,75 @@ function weekendcolor() {
     });
 }
 
+//#endregion
+
+
+//#region pang weekly report
+function validateWeekRange(mVal) {
+
+  var firstDay = new Date(`${mVal}-01`);
+  var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth() + 1, 0)
+
+  const selectedValue = $('#weekSel').val();
+
+  const selectedWeek = parseInt(selectedValue.slice(6));
+
+  // Define your allowed range
+  const allowedWeekRange = [getWeekNo(firstDay), getWeekNo(lastDay)]; // Adjust as needed
+
+  if (
+    selectedWeek >= allowedWeekRange[0] &&
+    selectedWeek <= allowedWeekRange[1]
+  ) {
+    highlightweek(selectedValue)
+  } else {
+    clearWeek()
+    alert('Selected week is outside this month\'s range.');
+  }
+}
+
+function getWeekNo(currentDate) {
+  var startDate = new Date(currentDate.getFullYear(), 0, 1);
+  var days = Math.floor((currentDate - startDate) /
+    (24 * 60 * 60 * 1000));
+  var weekNumber = Math.ceil(days / 7);
+  return weekNumber;
+}
+
+function clearWeek() {
+  $('#weekSel').val(0)
+}
+
+
+
 function highlightweek(wVal) {
+
+  $($('#w-total').nextAll()).remove()
+  $('#weekly-body').empty()
+  //add Emps
+  var empArr = []
+  $.each($('.plan-row'), function (indexInArray, valueOfElement) {
+    empArr.push({
+      empNum: $(valueOfElement).attr('emp-num'),
+      empName: $($(valueOfElement).children()[4]).text()
+    })
+  });
+  empArr = makeArrayUnique(empArr, 'empNum');
+
+  empArr.forEach(element => {
+    $('#weekly-body').append(`
+    <tr class="wp-row" get-from="plan-row" emp-num="${element.empNum}">
+    <td rowspan="2">${element.empName}</td>
+    <td>Plan</td>
+    <td rowspan="2" class="pa-percent"></td>
+    <td class="w-tot"></td>
+    </tr>
+    <tr class="wa-row" get-from="actual-row" emp-num="${element.empNum}">
+    <td>Actual</td>
+    <td class="w-tot"></td></tr>
+    `)
+  });
+
   const [inputYear, inputWeek] = wVal.split('-W');
   $('[date-val]').removeClass('bg-warning')
   const startDate = new Date(inputYear, 0, 1 + (inputWeek) * 7);
@@ -242,5 +290,43 @@ function highlightweek(wVal) {
     currentDate.setDate(startDate.getDate() + i);
     $(`[date-val="${currentDate.yyyymmdd()}"]`).addClass('bg-warning')
     daysInWeek.push(currentDate);
+    $('#weekly-headrow').append(`<th w-date-val="${currentDate.yyyymmdd()}">${currentDate.yyyymmdd()}</th>`);
+    $('.wp-row').append(`<td class="pa-details" w-date-val="${currentDate.yyyymmdd()}"></td>`);
+    $('.wa-row').append(`<td class="pa-details" w-date-val="${currentDate.yyyymmdd()}"></td>`);
   }
+
+  $.each($('.pa-details'), function (index, val) {
+    $(val).text(totalpa($($(val).parent()).attr('emp-num'), $(val).attr('w-date-val'), $($(val).parent()).attr('get-from')))
+  });
+
+  totalLeft()
+
 }
+
+function totalpa(empNum, date, pa) {
+  console.log(empNum, date, pa);
+  var arr = $(`.${pa}[emp-num="${empNum}"]`).children(`[date-val="${date}"]`);
+  var totalArr = [];
+
+  $.each(arr, function (index, val) {
+    totalArr.push($(val).text() == "" ? 0 : $(val).text())
+  });
+  return eval(totalArr.join('+')) == 0 ? "" : eval(totalArr.join('+'));
+}
+
+function totalLeft() {
+  $.each($('.w-tot'), function (index, val) {
+    var x = $(val).nextAll()
+    var arr = []
+    $.each(x, function (indexInArray, valueOfElement) {
+      arr.push($(valueOfElement).text() == "" ? 0 : $(valueOfElement).text())
+    });
+    $(val).text(eval(arr.join('+')))
+  });
+
+  $.each($('.pa-percent'), function (index, val) {
+    $(val).text(roundToDecimalPlaces(eval(`${$($(val).next()).text()}/${$($($($(val).parent()).next()).children('.w-tot')).text()}*100`), 2) + "%");
+  })
+}
+
+//#endregion
