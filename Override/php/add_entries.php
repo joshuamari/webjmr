b394b6a5a5438107785c50263fb4e25c0c808e83
Dave Reyes <reyes_d-kdt@global.kawasaki.com>
2024-09-03 14:23:56 +0800

    additional php, checkers

--- Override/php/add_entries.php
+++ Override/php/add_entries.php
@@ -1,6 +1,7 @@
 <?php
 #region DB Connect
 require_once "../../dbconn/dbconnectwebjmr.php";
+require_once "../../Override/php/global.php";
 #endregion
 
 #region set timezone
@@ -8,48 +9,51 @@ date_default_timezone_set('Asia/Manila');
 #endregion
 
 #region Initialize Variable
-if(!empty($_POST['grpNum'])){
-  $grpNum = $_POST['grpNum'];
-}
-else{
-  $msg['isSuccess'] = FALSE;
-  $msg['error'][] = "Group";
-}
-if(!empty($_POST['empNum'])) {
-  $empID = $_POST['empNum'];
-}
-else {
-  $msg['isSuccess'] = FALSE;
-  $msg['error'][] = "Employee No.";
-}
-if(!empty($_POST['setDate'])) {
-  $setDate = $_POST['setDate'];
-}
-else {
-  $msg['isSuccess'] = FALSE;
-  $msg['error'][] = "Date";
-}
-if(!empty($_POST['location'])) {
-  $location = $_POST['location'];
-}
-else {
-  $msg['isSuccess'] = FALSE;
-  $msg['error'][] = "Location";
-}
-if(!empty($_POST['projID'])){
-  $projID = $_POST['projID'];
-}
-else{
-  $msg['isSuccess'] = FALSE;
-  $msg['error'][] = "Project ID";
-}
+$required_fields = [
+  'empNum' => "Employee No.",
+  'grpNum' => "Group No.",
+  'selDate' => "Selected Date",
+  'locID' => "Location",
+  'projID' => "Project ID",
+  'itemID' => "Item ID",
+];
 
-$itemID = !empty($_POST['itemID']) ? $_POST['itemID'] : "";
-$addType = (!empty($_POST['addType'])) ? $_POST['addType'] : 0;
+$input = $_POST;
+$missing_fields = [];
+#endregion
+#initialize Session
+session_start();
+
+#region input checking region
+foreach ($required_fields as $key => $descrpition) {
+  if (empty($input[$key])) {
+    $missing_fields[] = $descrpition;
+  }
+}
+#endregion
+
+#region for separtion of error
+$count = count($missing_fields);
+if ($count > 0) {
+  if ($count === 1) {
+    $result['message'] = "{$missing_fields[0]} is missing.";
+  } elseif ($count === 2) {
+    $result['message'] = "{$missing_fields[0]} and {$missing_fields[1]} are missing.";
+  } else {
+    $last_field = array_pop($missing_fields);
+    $result['message'] = implode(', ', $missing_fields) . ", and $last_field are missing.";
+  }
+  die(json_encode($result));
+}
 #endregion
 
 #region ADDITIONAL CONDITION
-$grpAbbr = getGroup($grpNum);
+// $addType = (!empty($_POST['addType'])) ? $_POST['addType'] : 0;
+$jobReqDesc = (!empty($_POST['jobReqDesc'])) ? $_POST['jobReqDesc'] : null;
+
+
+
+
 #endregion
 
-echo $addType;
+echo json_encode($result);
--- Override/php/get_checkers.php
+++ Override/php/get_checkers.php
@@ -1,48 +1,81 @@
 <?php
-#region Require Database Connections
-require_once '../Includes/dbconnectkdtph.php';
-require_once "../Includes/dbconnectwebjmr.php";
+#region DB Connect
+require_once "../../dbconn/dbconnectwebjmr.php";
+require_once "../../dbconn/dbconnectkdtph.php";
+require_once "../../Override/php/global.php";
 #endregion
+
 #region Initialize Variables
-$output="<option value='' selected disabled>Select...</option>";
-$empNum='';
-if(isset($_REQUEST['empNum'])){
-    $empNum=$_REQUEST['empNum'];
+if(!empty($_POST['grpNum'])){
+  $grpNum = $_POST['grpNum'];
 }
-$empGrp='';
-if(isset($_REQUEST['empGrp'])){
-    $empGrp=$_REQUEST['empGrp'];
+else{
+  $msg['isSuccess'] = FALSE;
+  $msg['error'][] = "Group";
 }
-$projID='';
-if(!empty($_REQUEST['projID'])){
-    $projID=$_REQUEST['projID'];
-    if(!in_array($projID,$defaultProjID)){
-        $projGroupQ="SELECT fldGroup FROM projectstable WHERE fldID='$projID'";
-        $projGroupStmt=$connwebjmr->query($projGroupQ);
-        $projGroup=$projGroupStmt->fetchColumn();
-        $empGrp=$projGroup;
-    }
+if(!empty($_POST['empNum'])) {
+  $empID = $_POST['empNum'];
 }
-$sharedEmp="";
-$spQ="SELECT fldEmployeeNum FROM project_share WHERE fldProject='$projID'";
-$spStmt=$connwebjmr->query($spQ);
-if($spStmt->rowCount()>0){
-    $spArr=$spStmt->fetchAll();
+else {
+  $msg['isSuccess'] = FALSE;
+  $msg['error'][] = "Employee No.";
+}
+$grpAbbr = getGroup($grpNum);
+$projID = (!empty($_POST['projID'])) ? $_POST['projID'] : '';
+$defaultProjID = getDefaults();
+if(!in_array($projID,$defaultProjID)){
+  $projGroupQ = "SELECT fldGroup FROM projectstable WHERE fldID = :projID";
+  $projGroupStmt = $connwebjmr->prepare($projGroupQ);
+  $projGroupStmt->execute([":projID" => $projID]);
+  $projGroup = $projGroupStmt->fetchColumn();
+  $grpAbbr = $projGroup;
+}
+$sharedEmp = "";
+$sharedProjQ = "SELECT fldEmployeeNum FROM project_share WHERE fldProject = :projID";
+$sharedProjStmt = $connwebjmr->prepare($sharedProjQ);
+$sharedProjStmt->execute([":projID" => $projID]);
+if($sharedProjStmt->rowCount()>0){
+    $spArr = $sharedProjStmt->fetchAll();
     $arrValues = array_column($spArr, "fldEmployeeNum");
     $implodeString = implode("','",array_values($arrValues));
-    $sharedEmp="OR fldEmployeeNum IN ('" . $implodeString . "')";
+    $sharedEmp = "OR fldEmployeeNum IN ('" . $implodeString . "')";
 }
 #endregion
-#region Query Members
-$memQ = "SELECT * FROM emp_prof WHERE fldEmployeeNum<>'$empNum' AND (fldGroup='$empGrp' $sharedEmp) AND fldActive=1 AND fldNick<>''";
-$memStmt=$connkdt->query($memQ);
-$memArr=$memStmt->fetchAll();
-foreach($memArr AS $mem){
-    $memberid=$mem['fldEmployeeNum'];
-    $member=$mem['fldFirstname']." ".$mem['fldSurname'];
-    $output .= "<option class='member' dataid='$memberid'>$member</option>";
-}
 
+#region separtion of error
+if (!empty($msg)) {
+	if (count($msg['error']) > 1) {
+		$errorString = '';
+		foreach ($msg['error'] as $result) {
+			if ($result === end($msg['error'])) {
+				$errorString .= "and '$result' Missing";
+			} else {
+				$errorString .= "'$result', ";
+			}
+		}
+		$msg['error'] = $errorString;
+	} else {
+		$msg['error'] = implode("", $msg['error']);
+		$msg['error'] .= " Missing";
+	}
+	die(json_encode($msg));
+}
 #endregion
-echo $output;
-?>
+
+#region Main Query
+$memQ = "SELECT `fldEmployeeNum` AS `id`, CONCAT(`fldFirstName`, ' ', `fldSurname`) AS `name` FROM emp_prof WHERE fldEmployeeNum != :empNum AND (fldGroup = :empGrp :sharedEmp) AND fldActive = 1 AND fldNick != ''";
+$memStmt=$connkdt->prepare($memQ);
+$memStmt->execute([":empNum" => $empID, "empGrp" => $grpAbbr, ":sharedEmp" => $sharedEmp]);
+if($memStmt->rowCount() > 0) {
+  $result = $memStmt->fetchAll();
+  $msg['isSuccess'] = TRUE;
+  $msg['result'] = $result;
+  $msg['error'] = "Successfully retrieved!";
+}
+else{
+  $msg['isSuccess'] = FALSE;
+  $msg['error'] = "Retrieve Unsuccessfully!";
+}
+#endregion
+
+echo json_encode($msg);
